550396fa425aa69dc63bffa21535ddd9eee73332
korovyanskiy <korovyn@list.ru>
2022-06-07 14:50:10 +0300

    Initial Commit

---
+++ MyNewGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyNewGame
+//
+//  Created by Igor Korovyanskiy on 07.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
